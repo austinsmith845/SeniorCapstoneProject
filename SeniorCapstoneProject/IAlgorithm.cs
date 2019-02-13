2faeb322c4fb8d653cec497c2ca7864bddb179d7
austinsmith845 <46535698+austinsmith845@users.noreply.github.com>
2019-02-13 12:00:53 -0500

    Added IAlgorithm interface

---
+++ SeniorCapstoneProject/IAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorCapstoneProject
+{
+    /// <summary>
+    /// Acts as the contract for the algorithms to ensure that the NextMove() method is there.
+    /// </summary>
+    public interface IAlgorithm
+    {
+        void NextMove(RobotVacuum vacuum);
+        
+    }
+}
